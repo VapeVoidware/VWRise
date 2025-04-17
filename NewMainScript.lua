d6cdca76d5de3a59fc31bc2cae9e320b8481fae4
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-04-17 08:54:30 +0300

    Revert "Update NewMainScript.lua"
    This reverts commit 3e8c8a0f56199c0a60f4f4fbf90df2fca39645f4.

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -428,7 +428,7 @@ end
 local url = RiseMode and "https://github.com/VapeVoidware/VWRise/" or "https://github.com/VapeVoidware/VoidwareBakup"
 local commit = "main"
 writefile(baseDirectory.."commithash2.txt", commit)
-commit = 'e6e445b64999e54d3e10a144da07ae2e49925c29'
+commit = 'f193258bf7b9f417fefde0af1c9b25840c0c98e8'
 commit = shared.CustomCommit and tostring(shared.CustomCommit) or commit
 writefile(baseDirectory.."commithash2.txt", commit)
 local function vapeGithubRequest(scripturl, isImportant)
