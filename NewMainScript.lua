69948417e27c3c116a2ced5999f77200f2e37f68
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-07-07 22:15:16 +0300

    Invisibility, PlayerAttach & Damage Indicator! 🚀

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -428,7 +428,7 @@ end
 local url = RiseMode and "https://github.com/VapeVoidware/VWRise/" or "https://github.com/VapeVoidware/VoidwareBakup"
 local commit = "main"
 writefile(baseDirectory.."commithash2.txt", commit)
-commit = '3c66dea22b7830ecf31a7ba65d11143316c8feb1'
+commit = 'f4e1d749df77f338a848362715695479d58b1dce'
 commit = shared.CustomCommit and tostring(shared.CustomCommit) or commit
 writefile(baseDirectory.."commithash2.txt", commit)
 local function vapeGithubRequest(scripturl, isImportant)
