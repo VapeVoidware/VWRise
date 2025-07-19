94e8c109b313f03b5876c12d312a5470f8f3c77e
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-07-19 11:21:00 +0300

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -428,7 +428,7 @@ end
 local url = RiseMode and "https://github.com/VapeVoidware/VWRise/" or "https://github.com/VapeVoidware/VoidwareBakup"
 local commit = "main"
 writefile(baseDirectory.."commithash2.txt", commit)
-commit = '16dae240d547053eafce62f3ab9ec7774b3d59f2'
+commit = '139ef7f3eb0340a86c4eebb7efd7504562a00326'
 commit = shared.CustomCommit and tostring(shared.CustomCommit) or commit
 writefile(baseDirectory.."commithash2.txt", commit)
 local function vapeGithubRequest(scripturl, isImportant)
