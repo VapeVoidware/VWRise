af8595bfff2cc8935ddd502d828342c7025e8e2a
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-12-02 22:32:27 +0200

    Upd

--- CustomModules/CE6872274481.lua
+++ CustomModules/CE6872274481.lua
@@ -9347,6 +9347,13 @@ run(function()
 	end
 
 	local AutoKit_Functions = {
+		pinata = function()
+			kitCollection(lplr.Name..':pinata', function(v)
+				if getItem('candy') then
+					bedwars.Client:Get("DepositCoins"):CallServer(v)
+				end
+			end, 6, true)
+		end,
 		hannah = function()
 			kitCollection('HannahExecuteInteraction', function(v)
 				local billboard = bedwars.Client:Get("HannahPromptTrigger"):InvokeServer({
